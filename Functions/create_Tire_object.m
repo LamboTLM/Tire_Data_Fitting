d995535e0b6bdc462fe757e4d3d72d3408550084
DESKTOP-O3F46KV\Danie <Daniel.Lambrecht@dynamics-regensburg.de>
2026-07-11 00:18:39 +0200

    removed old unneded data

--- Functions/create_Tire_object.m
+++
@@ -1,7 +0,0 @@
-function [tdnew] = create_Tire_object
-% Erstellt ein tireData Object im SAE Koordinaten system
-
-tdnew = tireData();                         % Anlegen des Objects
-tdnew = tdnew.coordinateTransform("SAE");   % Definition des Koordinaten systems, später überschrieben
-
-end
